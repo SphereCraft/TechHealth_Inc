0d37023518abb6c8ca93bce654314a7495742ba8
Jeff Webb <j.archerwebb@gmail.com>
2024-12-21 17:21:39 +0000

    Initial commit

---
+++ test/tech_health_inc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechHealthInc from '../lib/tech_health_inc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tech_health_inc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechHealthInc.TechHealthIncStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
